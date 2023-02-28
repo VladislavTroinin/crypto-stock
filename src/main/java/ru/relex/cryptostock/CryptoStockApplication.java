31415ecf209c3c1f650ac2ba819c5afdf6e5af93
Vladislav Troynin <vladislav.troynin@mirapolis.ru>
2023-02-28 13:54:57 +0300

    Initial commit

---
+++ src/main/java/ru/relex/cryptostock/CryptoStockApplication.java
@@ -0,0 +1,13 @@
+package ru.relex.cryptostock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoStockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoStockApplication.class, args);
+    }
+
+}
